5906bcb5ccc29c82d308507e576a5d5a4f37da5a
AymanDev <maxsa21@gmail.com>
2019-05-14 02:05:37 +0700

    Replaced grid with segment

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import 'semantic-ui-css/semantic.min.css'
-import { Button, Container, Segment, Grid } from 'semantic-ui-react';
+import { Button, Container, Segment, Grid, Form, Header } from 'semantic-ui-react';
 import { Link } from 'react-router-dom'
 
 interface PageInterface {
@@ -10,16 +10,27 @@ interface PageInterface {
 class Index extends React.Component<PageInterface, {}> {
     render() {
         return (
-            <Segment inverted vertical masthead>
-                <Grid stackable>
-                    <Grid.Column textAlign='center'>
-                        <h1>Welcome to React with Typescript</h1>
-                        <p>The color of this page is: {this.props.color}</p>
-                        <Button content="Click me hards as you can!" />
-                    </Grid.Column>
-                </Grid>
-            </Segment>
+            <div>
+                <Segment inverted vertial textAlign="center">
+                    <p>Welcome to React with Typescript</p>
+                    <p>The color of this page is: {this.props.color}</p>
+                    <Button content="Click me hards as you can!" />
+                </Segment>
 
+                <Container>
+                    <Form>
+                        <Form.Field>
+                            <label>Login:</label>
+                            <input type="text" placeholder="Login" minLength={3} maxLength={13} />
+                        </Form.Field>
+                        <Form.Field>
+                            <label>Password:</label>
+                            <input type="password" placeholder="Password" minLength={6} maxLength={32} />
+                        </Form.Field>
+                        <Button type="submit">Log-in</Button>
+                    </Form>
+                </Container>
+            </div>
         );
     }
 }
